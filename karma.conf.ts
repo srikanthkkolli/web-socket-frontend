94374862c47be393e415668143c9bfdd5a9e301d
Srikanth Krishna Kolli <ksrikanth.kolli@gmail.com>
2025-05-02 02:03:18 -0700

    tt

---
+++ karma.conf.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    autoWatch: false
+  });
+};
