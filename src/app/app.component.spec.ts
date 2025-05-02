67c38bbddf4cbf4254ad8d174427c3ffb2f41126
Srikanth Krishna Kolli <ksrikanth.kolli@gmail.com>
2025-05-02 00:19:31 -0700

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {Component} from '@angular/core';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+
+@Component({selector: 'app-bingo', template: ''})
+class StubBingoComponent {
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +14,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        StubBingoComponent
       ],
     }).compileComponents();
   });
@@ -24,12 +30,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('web-socket-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, web-socket-frontend');
   });
 });
