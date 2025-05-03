5bcb1118ca8a89a179515c0e45c92966cb215548
Srikanth Krishna Kolli <ksrikanth.kolli@gmail.com>
2025-05-02 19:14:30 -0700

    test-1 (#5)
    * a
    
    * a
    
    * a

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
+import {provideRouter} from '@angular/router';
 import {AppComponent} from './app.component';
 
 @Component({selector: 'app-bingo', template: ''})
@@ -10,9 +10,7 @@ class StubBingoComponent {
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+      providers: [provideRouter([])],
       declarations: [
         AppComponent,
         StubBingoComponent
@@ -29,6 +27,6 @@ describe('AppComponent', () => {
   it(`should have as title 'web-socket-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('web-sodcket-frontend');
+    expect(app.title).toEqual('web-socket-frontend');
   });
 });
