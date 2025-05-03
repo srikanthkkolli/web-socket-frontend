b98e8310dbd3de3c078033d861a83e70ec1650b9
Srikanth Krishna Kolli <ksrikanth.kolli@gmail.com>
2025-05-02 19:11:10 -0700

    a

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
+import {provideRouter} from '@angular/router';
 import {AppComponent} from './app.component';
 
 @Component({selector: 'app-bingo', template: ''})
@@ -10,9 +10,7 @@ class StubBingoComponent {
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+      providers: [provideRouter([])],
       declarations: [
         AppComponent,
         StubBingoComponent
